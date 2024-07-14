14b2e7cfabd872b604ad36d8b70325c13ff7e519
Bilal <sba.comp.professional>
2024-07-15 01:16:25 +0530

    app moved to git

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("node started");
+
+
+var a=2;
+var b=31;
+
+var c=a+b;
+
+console.log("sum is :"+c);
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);  // Print the value of i
+}
+
+
+
+let sales=[21, 20, 23, 26];
+
+let totalsales=0;
+
+
+for ( let i=0; i < sales.length; i++)
+    
+    {
+    totalsales+=sales[i];
+}
+
+console.log(totalsales);
